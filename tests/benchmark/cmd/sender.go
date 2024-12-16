ae68af1606e7aea200d67c466d88041ddf57d28e
Solovyov1796 <xinyu@0g.ai>
2024-12-16 10:54:57 +0800

    add log

--- tests/benchmark/cmd/sender.go
+++ tests/benchmark/cmd/sender.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/0glabs/0g-chain/tests/benchmark/generator"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -25,8 +26,8 @@ func (s *Sender) Send() {
 		err := s.Client.SendTransaction(ctx, t)
 		if err != nil {
 			panic(fmt.Sprintln("Failed to send transactions ", t.Hash().String(), " error: ", err.Error()))
-			// } else {
-			// 	println(time.Now().Format("2006-01-02 15:04:05.000000"), ">>> ", "Sent transaction", t.Hash().String())
+		} else {
+			println(time.Now().Format("2006-01-02 15:04:05.000000"), ">>> ", "Sent transaction", t.Hash().String())
 		}
 	}
 }
